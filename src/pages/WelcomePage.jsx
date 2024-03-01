644d2144f72853ebbb02f65d8b234c7c4bdfd86e
Ganjana <ganjana.thinpho@utb.ecutbildning.se>
2024-03-01 14:53:02 +0100

    Submit Name, Validate Form Input, useNavigate to ToDoPage

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,17 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "../pages/welcome-page.css";
+import { useNavigate } from "react-router-dom";
 
 function WelcomePage() {
+  const [userName, setUserName] = useState("");
+  const [formError, setFormError] = useState("");
+  const navigate = useNavigate();
+
+  function handleChange(e){
+    setUserName(prev => prev = (e.target.value))
+    setFormError("")
+  }
+
+  function validateForm(){
+    let error = "";
+    if (userName === ""){
+     error = "Please enter name"
+    }
+    return error;
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+
+    let error = validateForm();
+    setFormError(error);
+    if (error === ""){
+      navigate("/todo", {state: {userName}})
+    }
+  }
   return (
     <>
-    <h1>Do it.</h1>
+      <h1>Do it.</h1>
       <section className="container name-container">
         <h2>Start your journey here</h2>
-        <form className="name-form">
+        <form onSubmit={handleSubmit} className="name-form">
+          {formError && <p className="error">{formError}</p>}
           <label htmlFor="name">
-            <input type="text" name="name" id="name" placeholder="Enter your name" />
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={userName}
+              onChange={handleChange}
+              placeholder="Enter your name"
+            />
           </label>
-          <button className="style-btn" type="submit">
+          <button type="submit" className="style-btn" >
             Create list
           </button>
         </form>
